ec812eaadc9e18b6cb100cc93e135846be4994d0
Fillipe Hott <fillipehottm@gmail.com>
2016-08-11 10:50:41 -0300

    Add bootstrap / separete adds forms

--- client/main.js
+++ client/main.js
@@ -4,14 +4,30 @@ import '../imports/ui/body.js';
 import '../imports/ui/task.html';
 import '../imports/ui/task.js';
 
+import '../imports/ui/forms/add-straw.html';
+import '../imports/ui/forms/add-wood.html';
+import '../imports/ui/forms/add-brick.html';
+
 FlowRouter.route('/', {
   action: function(params) {
     BlazeLayout.render("main");
   }
 });
 
+FlowRouter.route('/add-straw', {
+  action: function(params) {
+    BlazeLayout.render("addStraw");
+  }
+});
+
 FlowRouter.route('/add-wood', {
   action: function(params) {
     BlazeLayout.render("addWood");
+  }
+});
+
+FlowRouter.route('/add-brick', {
+  action: function(params) {
+    BlazeLayout.render("addBrick");
   }
 });
